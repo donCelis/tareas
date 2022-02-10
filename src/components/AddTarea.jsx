1babf587b7c05c8f7e431b46f905d52a35dc0bd1
Alexander Celis <dacelis0@misena.edu.co>
2022-02-09 19:49:11 -0500

    add: useContext

---
+++ src/components/AddTarea.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const AddTarea = ({ onAdd }) => {
+
+	const [texto, setTexto] = useState('')
+	const [fecha, setFecha] = useState('')
+	const [terminado, setTerminado] = useState(false)
+
+	const onSubmit = (e) => {
+
+		e.preventDefault()
+
+		if (!texto) {
+			alert('Por favor agrega la descripcion de la Tarea')
+			return
+		}
+
+		onAdd({ texto, fecha, terminado })
+
+		setTexto('')
+		setFecha('')
+		setTerminado(false)
+
+	}
+
+	return (
+		<form className='add-form' onSubmit={onSubmit}>
+			<div className='form-control'>
+				<label>Tarea</label>
+				<input type='text' value={texto} onChange={(e) => setTexto(e.target.value)} placeholder='Agregar Tarea' />
+			</div>
+			<div className='form-control'>
+				<label>Fecha</label>
+				<input type='text' value={fecha} onChange={(e) => setFecha(e.target.value)} placeholder='Fecha' />
+			</div>
+			<div className='form-control form-control-check'>
+				<label>Terminado</label>
+				<input type='checkbox' checked={terminado} value={terminado} onChange={(e) => setTerminado(e.currentTarget.checked)} />
+			</div>
+			<input type='submit' value='Guardar Tarea' className="btn btn-block" />
+		</form>
+	);
+};
+
+export default AddTarea;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { FaTasks } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Button from './Button';
+
+const Header = ({ title, onAdd, showAdd }) => {
+
+	const agregar = () => {
+		console.log('agregar')
+	}
+
+	const eliminar = () => {
+		console.log('eliminar')
+	}
+
+	return (
+		<header className='header'>
+			<h1><FaTasks /> {title}</h1>
+			<Button color={showAdd ? 'blue' : 'green'} text={showAdd ? 'Cerrar' : 'Agregar'} onClick={onAdd} />
+		</header>
+	);
+};
+
+// Header.defaultProps = {
+// 	title: 'Titulo por defecto'
+// }
+
+Header.propTypes = {
+	title: PropTypes.string.isRequired,
+}
+
+// const estilos = {
+// 	color: 'red',
+// 	backgroundColor: 'black',
+// }
+
+export default Header;
+
